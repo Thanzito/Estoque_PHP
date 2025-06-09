68dd7b22dc72bcccd9fcf0435bacd97c003ffe61
Thanzito <163420206+Thanzito@users.noreply.github.com>
2025-06-09 12:05:25 -0300

    Add files via upload

---
+++ listar_produtos.php
@@ -0,0 +1,21 @@
+<?php
+header('Content-Type: application/json');
+
+//Conectar o banco
+$conn = new mysqli ("localhost","root","","panificadora");
+
+if($conn->connect_error) {
+    die(json_encode(["erro" => "Erro ao conectar"]));
+};
+
+//Consulta SQL
+$sql = "SELECT id, nome, quantidade, preco, FROM produtos";
+$resultado = $conn -> query($sql);
+
+$produtos = [];
+
+while($linha = $resultado -> fetch_assoc()){
+    $produtos[] = $linha;
+}
+
+echo json_encode($produtos);
